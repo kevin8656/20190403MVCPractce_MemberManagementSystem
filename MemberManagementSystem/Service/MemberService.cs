6ca3f5f012dcab0dc1634bb4ca3627f038f4e626
kevinhuang <kevin8656@hotmail.com>
2019-04-14 23:15:40 +0800

    Add MemberService And Add GetAllMember() Function

---
+++ MemberManagementSystem/Service/MemberService.cs
@@ -0,0 +1,18 @@
+﻿using MemberManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemberManagementSystem.Service
+{
+    public class MemberService
+    {
+        Models.MemberManagementSystemEntities db = new Models.MemberManagementSystemEntities();
+
+        public List<Member> GetAllMember()
+        {
+            return db.Member.ToList();
+        }
+    }
+}
