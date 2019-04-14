9ecef9dc2f241272ec09c381e7f8a202ee0d56dc
kevinhuang <kevin8656@hotmail.com>
2019-04-14 23:18:05 +0800

    Add MemberController And Use Action To Create A View /Member/Index.cshtml

---
+++ MemberManagementSystem/Controllers/MemberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MemberManagementSystem.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
