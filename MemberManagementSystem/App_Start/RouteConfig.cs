8379c9ecbd731947629c8d99193b04b69b976cf9
Kevin Huang <kevin8656@hotmail.com>
2019-04-20 10:55:15 +0800

    Setting route config and fix model problem

--- MemberManagementSystem/App_Start/RouteConfig.cs
+++ MemberManagementSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MemberManagementSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Member", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
